24de94551a60ca9e23118a08aecc972cb52e00e9
Deep <deep.shah@bacancy.com>
2023-02-26 11:48:45 -0500

    User and Company details

---
+++ Task2.rb
@@ -0,0 +1,20 @@
+class User 
+  CONSTANT='surat'
+  def initialize(user)
+  @@user=user
+  end    
+end
+class Company < User
+  def initialize(company)
+  @company=company
+  end
+  def display
+  puts "#{CONSTANT}"
+  puts "#{@company}"
+  puts "#{@@user}"
+  end
+end
+user=User.new('deep')
+company=Company.new('google')
+company.display
+
