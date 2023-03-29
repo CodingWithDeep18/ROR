d6ed54f1ba0773ffd6dc38c684278fbd9e7fde1d
CodingWithDeep18 <126406127+CodingWithDeep18@users.noreply.github.com>
2023-03-29 11:11:47 +0530

    Update development.rb

--- mailer/config/environments/development.rb
+++ mailer/config/environments/development.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'deepshah4994@gmail.com',
-    :password             => 'ywjrlcxrpjwnmiew',
+    :user_name            => ,
+    :password             => ,
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
